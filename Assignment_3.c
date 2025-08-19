d0920897539bf352d2f6b42260bc4064a84edca3
KomalChoudhury <choudhuryk363@gmail.com>
2025-08-19 20:31:53 +0530

    Update and rename Program4.2.c to Assignment_3.c

---
+++ Assignment_3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,result;
+    printf("Enter first integer:");
+    scanf("%f",&num1);
+    printf("Enter second integer:");
+    scanf("%f",&num2);
+    result = num1-num2;
+    printf("result:%d\n",result);
+    return 0;
+
+}
