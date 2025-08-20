b5a931913b2c625b70df6768a9f801981110b03f
KomalChoudhury <choudhuryk363@gmail.com>
2025-08-20 21:39:16 +0530

    Add files via upload

---
+++ Assignment_7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	float radius,area;
+	printf("Enter the radius of the circule: \n");
+	scanf("%f", &radius);
+	area = 3.4*radius*radius;
+	printf("The area of circule is: %f", area);
+	return 0;
+}
