9ab4bf4476fa7530fa5c0be39ccea793b985d02e
KomalChoudhury <choudhuryk363@gmail.com>
2025-08-20 21:24:21 +0530

    Update Assignment_4.c

--- Assignment_4.c
+++ Assignment_4.c
@@ -3,16 +3,17 @@ int main(){
     int num;
     printf("enter a number:");
     scanf("%d",&a);
-    printf(" %d*1=%d\n",num,num*1);
-    printf(" %d*2=%d\n",num,num*2);
+    printf("%d*1=%d\n",num,num*1);
+    printf("%d*2=%d\n",num,num*2);
     printf("%d*3=%d\n",num,num*3);
-    printf(" %d",a*4);
-    printf(" %d",a*5);
-    printf(" %d",a*6);
-    printf(" %d",a*7);
-    printf(" %d",a*8);
-    printf(" %d",a*9);
-    printf(" %d",a*10);
+    printf("%d*4=%d\n",num,num*4);
+    printf("%d*5=%d\n",num,num*5);
+    printf("%d*6=%d\n",num,num*6);
+    printf("%d*7=%d\n",num,num*7);
+    printf("%d*8=%d\n",num,num*8);
+    printf("%d*9=%d\n",num,num*9);
+    printf("%d*10=%d\n",num,num*10);
     return 0;
 
 }
+
