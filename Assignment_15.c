0dee6eec491d355f3421552e0e1c5c7c5b966a91
KomalChoudhury <choudhuryk363@gmail.com>
2025-08-27 00:07:38 +0530

    Update Assignment_15.c

--- Assignment_15.c
+++ Assignment_15.c
@@ -1,43 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-
-int main() {
-    double a, b, c, disriminatinant, root1, root2, realPart, imagPart;
-
+int main(){
+  floata,b,c,d,root1,root2;
     
     printf("Enter coefficients a, b and c: ");
     scanf("%lf %lf %lf", &a, &b, &c);
+    d = b*b - 4*a*c;
+    root1 = (-b + sqrty (d)) / (2*a);
+    root2 = (-b - sqrt(d)) / (2*a);
 
-    if (a == 0) {
-        printf("Not a quadratic equation (a cannot be 0).\n");
-        return 0;
-    }
-
-    d = b * b - 4 * a * c;  
-
-    if (discriminant > 0) {
-        
-        root1 = (-b + sqrt(d)) / (2 * a);
-        root2 = (-b - sqrt(d)) / (2 * a);
-        printf("Roots are real and different:\n");
-        printf("Root1 = %.2lf\n", root1);
-        printf("Root2 = %.2lf\n", root2);
-    }
-    else if (discriminant == 0) {
-        
-        root1 = -b / (2 * a);
-        printf("Roots are real and equal:\n");
-        printf("Root1 = Root2 = %.2lf\n", root1);
-    }
-    else {
-        
-        realPart = -b / (2 * a);
-        imagPart = sqrt(-d) / (2 * a);
-        printf("Roots are complex and different:\n");
-        printf("Root1 = %.2lf + %.2lfi\n", realPart, imagPart);
-        printf("Root2 = %.2lf - %.2lfi\n", realPart, imagPart);
-    }
-
+    printf("Root = %.2f\n", root1);
+    printf("Root = %.2f\n", root2);
     return 0;
 }
+
+
+
+
 
