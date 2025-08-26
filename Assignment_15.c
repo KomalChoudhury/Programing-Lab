6b17b7ecec5f160750bde29d76ece0d69ae50a8e
KomalChoudhury <choudhuryk363@gmail.com>
2025-08-26 23:58:23 +0530

    Add files via upload

---
+++ Assignment_15.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, d, root1, root2, realPart, imagPart;
+
+    // Input coefficients
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (a == 0) {
+        printf("Not a quadratic equation (a cannot be 0).\n");
+        return 0;
+    }
+
+    d = b * b - 4 * a * c;  
+
+    if (d > 0) {
+        
+        root1 = (-b + sqrt(d)) / (2 * a);
+        root2 = (-b - sqrt(d)) / (2 * a);
+        printf("Roots are real and different:\n");
+        printf("Root1 = %.2lf\n", root1);
+        printf("Root2 = %.2lf\n", root2);
+    }
+    else if (d == 0) {
+        
+        root1 = -b / (2 * a);
+        printf("Roots are real and equal:\n");
+        printf("Root1 = Root2 = %.2lf\n", root1);
+    }
+    else {
+        
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-d) / (2 * a);
+        printf("Roots are complex and different:\n");
+        printf("Root1 = %.2lf + %.2lfi\n", realPart, imagPart);
+        printf("Root2 = %.2lf - %.2lfi\n", realPart, imagPart);
+    }
+
+    return 0;
+}
