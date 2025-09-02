88be94fb51df317a05bbf012c30aeeeb8580d6cf
KomalChoudhury <choudhuryk363@gmail.com>
2025-09-02 22:24:22 +0530

    Add files via upload

---
+++ Assignment_16.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int roll;
+	printf("Enter roll number: ");
+	scanf("%d", &roll);
+	if(roll == 100){
+    	printf("roll 100 is present");
+	}
+	if(roll !=100){
+		printf("roll 100 is not present"
+		);
+	}
+	return 0;
+}
