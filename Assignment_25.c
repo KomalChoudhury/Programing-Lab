2a55d15db45805152d8a338dacf9656cf81f8f26
KomalChoudhury <choudhuryk363@gmail.com>
2025-09-03 21:16:39 +0530

    Add files via upload

---
+++ Assignment_25.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("Enter the value of a,b,c");
+	scanf("%d %d %d", &a,&b,&c);
+	if(a>b){
+		if(a > c)
+		printf("a is maximum");
+		else 
+		printf("c is maximum");
+		}else{
+			if(b > c)
+			printf("b is maximum");
+			else
+			printf("c is maximum");
+		}
+		return 0;
+}
