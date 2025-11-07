1c78dd349f2aeb1441d51b9cc2d398dee595af23
KomalChoudhury <choudhuryk363@gmail.com>
2025-11-07 19:44:04 +0530

    Update Assignment_36.c

--- Assignment_36.c
+++ Assignment_36.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
-int main () {
-   int i,j;
-   char ch='A';
-
-   for (i=1; i<=4; i++){
-    for (j=i ; j<4; j++) {
-        printf (" ");
-    }
-    for (j=1; j<=i; j++){
-        printf ("%c", ch++);
-    }
-    printf ("\n");
-}
-    return 0;
+#include<stdio.h>
+int main(){
+	int i,j,n=4;
+	for(i=0;i<=n;i++){
+		for(j=n;j>i;j--){
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+	return 0;
 }
