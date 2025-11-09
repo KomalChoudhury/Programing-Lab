d5a0351b339d1552a709481ef8da1b4714bb4c56
KomalChoudhury <choudhuryk363@gmail.com>
2025-11-09 11:58:08 +0530

    Update Assignment_11.c

--- Assignment_11.c
+++ Assignment_11.c
@@ -3,9 +3,10 @@ int main(){
 	    int num,num1,num2;
 	    printf("Enter an integer:\n");
 	    scanf("%d", &num);
-	     num1 = num1 % 10;
+	     num1 = num % 10;
 	     num2 = num - (num/10)*10;
 	     printf("num (using modulas) = %d\n", num1);
 	     printf("num (wihtout using modulus) = %d\n", num2);
 	     return 0;
 }
+
