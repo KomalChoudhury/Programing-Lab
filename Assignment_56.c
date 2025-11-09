393e72adcc04ac2d38236df0287ac08c5e203ddc
KomalChoudhury <choudhuryk363@gmail.com>
2025-11-09 20:04:36 +0530

    Update Assignment_56.c

--- Assignment_56.c
+++ Assignment_56.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 int fib(int n){
-if(n = 0){
+if(n == 0){
 	return 0;
 }	
-if(n = 1){
+else if(n == 1){
 	return 1;
 }
+else{
+
 int fibn1 = fib(n - 1);
 int fibn2 = fib(n - 2);
 int fibN  = fibn1 + fibn2;
 return fibN;
+}
 }
 
 
